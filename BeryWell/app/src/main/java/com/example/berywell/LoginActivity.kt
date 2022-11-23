6f4bb5450e3f3eebfbe3bcf1371c91c54a42b4fa
lh99j <grand2181@gmail.com>
2022-11-23 16:30:21 +0900

    project settings

---
+++ BeryWell/app/src/main/java/com/example/berywell/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.berywell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.example.berywell.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val toolbar : Toolbar = binding.loginTb
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
