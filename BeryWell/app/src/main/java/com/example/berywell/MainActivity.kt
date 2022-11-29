4730c0a6fff49ad2698ec38de65c15833adbf2c2
lh99j <grand2181@gmail.com>
2022-11-29 17:11:56 +0900

    Main UI 수정

--- BeryWell/app/src/main/java/com/example/berywell/MainActivity.kt
+++ BeryWell/app/src/main/java/com/example/berywell/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.berywell
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import com.example.berywell.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val toolbar : Toolbar = binding.mainTb
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu, menu)
+//
+//        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+//        (menu!!.findItem(R.id.main_search_it).actionView as SearchView).apply {
+//            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+//        }
+//
+//        return true
+//    }
+
+
 }
